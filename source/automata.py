464fe4b9aa1d90298d0af13d3ab40df42bf88a7d
Alejandro Piad <alepiad@gmail.com>
2018-10-15 10:08:31 -0400

    Conf 6

--- source/automata.py
+++ source/automata.py
@@ -6,18 +6,19 @@ from .base import Graph
 
 
 class Automaton(Graph):
-    def __init__(self, start, final, states):
+    def __init__(self, start, final, states, shape='circle'):
         self.start = start
         self.states = states
         self.final = final
+        self._shape = shape
 
     def graph(self):
         G = pydot.Dot(rankdir='LR', margin=0.1)
         G.add_node(pydot.Node('start', shape='plaintext', label='', width=0, height=0))
 
         for (start, tran), end in self.states.items():
-            G.add_node(pydot.Node(start, shape='circle', style='bold' if start in self.final else ''))
-            G.add_node(pydot.Node(end, shape='circle', style='bold' if end in self.final else ''))
+            G.add_node(pydot.Node(start, shape=self._shape, style='bold' if start in self.final else ''))
+            G.add_node(pydot.Node(end, shape=self._shape, style='bold' if end in self.final else ''))
             G.add_edge(pydot.Edge(start, end, label=tran, labeldistance=2))
 
         G.add_edge(pydot.Edge('start', self.start, label='', style='dashed'))
